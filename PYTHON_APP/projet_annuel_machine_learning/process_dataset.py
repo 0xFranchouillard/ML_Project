5636007b1d8f0f2bf0a9937183e2a17f23b1ad82
CyrilleChampion <cyrillechampion13@gmail.com>
2021-06-01 19:55:40 +0200

    resize & load dataset function

--- PYTHON_APP/projet_annuel_machine_learning/process_dataset.py
+++ PYTHON_APP/projet_annuel_machine_learning/process_dataset.py
@@ -1,18 +1,19 @@
 import os
 import numpy as np
-from matplotlib import pyplot as plt
 from PIL import Image
 
 PATH = os.path.realpath('../../DATASET')
 
-if __name__ == '__main__':
+def resize_dataset(x, y):
+    filenames = [f for f in os.listdir(PATH)]
+    for f in filenames:
+        image = Image.open(PATH + '\\' + f).resize((x, y))
+        image.save(PATH + '\\' + f)
+
+def load_dataset():
     filenames = [f for f in os.listdir(PATH)]
     dataset = []
-    x = 500
-    y = 500
     for f in filenames:
-        f = np.array(Image.open(PATH + '\\' + f).resize((x, y)))
+        f = np.array(Image.open(PATH + '\\' + f))
         dataset.append(f)
-    for i in dataset:
-        plt.imshow(i)
-        plt.show()
+    return dataset
