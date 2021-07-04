24fdb98584957cc50f126be4db2b6f3b77ae1848
CyrilleChampion <cyrillechampion13@gmail.com>
2021-07-04 12:17:21 +0200

    training & testing dataset splitting random

--- PYTHON_APP/projet_annuel_machine_learning/process_dataset.py
+++ PYTHON_APP/projet_annuel_machine_learning/process_dataset.py
@@ -5,6 +5,7 @@ import threading
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import random
 
 PATH = os.path.realpath('../../DATASET')
 FILENUMBER = 0
@@ -37,35 +38,45 @@ def resize_dataset(x, y):
         threads[i].start()
 
 
-def load_image(filepath):
-    f = np.array(Image.open(filepath))
-    return f
+def load_image(filepath, train_ratio):
+    if random.uniform(0, 1) > train_ratio:
+        return np.array(Image.open(filepath)), False
+    return np.array(Image.open(filepath)), True
 
 
-def load_images(label, dataset_img, dataset_label):
+def load_images(label, dataset_img, dataset_label, train_ratio, testing_dataset_img, testing_dataset_label):
     filenames = [f for f in os.listdir(PATH + '\\' + label)]
     global CPT
     for f in filenames:
-        dataset_img.append(load_image(PATH + '\\' + label + '\\' + f))
-        dataset_label.append(label)
+        image, training = load_image(PATH + '\\' + label + '\\' + f, train_ratio)
+        if training:
+            dataset_img.append(image)
+            dataset_label.append(label)
+        else:
+            testing_dataset_img.append(image)
+            testing_dataset_label.append(label)
         CPT += 1
         print(f'{CPT / FILENUMBER:2.2%} fichiers chargés')
 
 
-def load_dataset():
+def load_dataset(train_ratio):
     global CPT
     global FILENUMBER
     CPT = 0
     FILENUMBER = 0
     labelnames = [f for f in os.listdir(PATH)]
-    dataset_img = []
-    dataset_label = []
+    training_dataset_img = []
+    training_dataset_label = []
+    testing_dataset_img = []
+    testing_dataset_label = []
     threads = [None] * len(labelnames)
+
     for i in range(len(threads)):
         FILENUMBER += len(fnmatch.filter(os.listdir(PATH + '\\' + labelnames[i] + '\\'), '*.*'))
-        threads[i] = threading.Thread(target=load_images, args=(labelnames[i], dataset_img, dataset_label))
+        threads[i] = threading.Thread(target=load_images, args=(labelnames[i], training_dataset_img, training_dataset_label,train_ratio, testing_dataset_img, testing_dataset_label))
         threads[i].start()
     for i in range(len(threads)):
         threads[i].join()
-    return dataset_img, dataset_label
+
+    return training_dataset_img, training_dataset_label, testing_dataset_img, testing_dataset_label
 
