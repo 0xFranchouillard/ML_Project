281ca717014ff65e7729dd51cc9c8ae01cb085f1
CyrilleChampion <cyrillechampion13@gmail.com>
2021-07-10 16:45:29 +0200

    split dataset change load_dataset function

--- PYTHON_APP/projet_annuel_machine_learning/process_dataset.py
+++ PYTHON_APP/projet_annuel_machine_learning/process_dataset.py
@@ -1,82 +1,82 @@
 import fnmatch
 import os
 import threading
-
 import matplotlib.pyplot as plt
+import numpy
 import numpy as np
 from PIL import Image
 import random
 
-PATH = os.path.realpath('../../DATASET')
 FILENUMBER = 0
 CPT = 0
 
 
-def resize_image(filepath, x, y):
-    image = Image.open(filepath).resize((x, y))
+def resize_image(filepath, x):
+    image = Image.open(filepath)
+    width, height = image.size
+    ratio = float(width / height)
+    image = image.resize((x, int(x * ratio)))
     image.save(filepath)
 
 
-def resize_images(label, x, y):
-    filenames = [f for f in os.listdir(PATH + '\\' + label)]
+def resize_images(path, label, x):
+    filenames = [f for f in os.listdir(path + '\\' + label)]
     global CPT
     for f in filenames:
-        resize_image(PATH + '\\' + label + '\\' + f, x, y)
+        resize_image(path + '\\' + label + '\\' + f, x)
         CPT += 1
         print(f'{CPT / FILENUMBER:2.2%} fichiers resizés')
 
 
-def resize_dataset(x, y):
+def resize_dataset(path, x):
     global CPT, FILENUMBER
     CPT = 0
     FILENUMBER = 0
-    labelnames = [f for f in os.listdir(PATH)]
+    labelnames = [f for f in os.listdir(path)]
     threads = [None] * len(labelnames)
     for i in range(len(threads)):
-        FILENUMBER += len(fnmatch.filter(os.listdir(PATH + '\\' + labelnames[i] + '\\'), '*.*'))
-        threads[i] = threading.Thread(target=resize_images, args=(labelnames[i], x, y))
+        FILENUMBER += len(fnmatch.filter(os.listdir(path + '\\' + labelnames[i] + '\\'), '*.*'))
+        threads[i] = threading.Thread(target=resize_images, args=(labelnames[i], x))
         threads[i].start()
 
 
-def load_image(filepath, train_ratio):
-    if random.uniform(0, 1) > train_ratio:
-        return np.array(Image.open(filepath)), False
-    return np.array(Image.open(filepath)), True
+def load_image(filepath):
+    return np.array(Image.open(filepath))
 
 
-def load_images(label, dataset_img, dataset_label, train_ratio, testing_dataset_img, testing_dataset_label):
-    filenames = [f for f in os.listdir(PATH + '\\' + label)]
+def load_images(path, label, dataset_img, dataset_label):
+    filenames = [f for f in os.listdir(path + '\\' + label)]
     global CPT
     for f in filenames:
-        image, training = load_image(PATH + '\\' + label + '\\' + f, train_ratio)
-        if training:
-            dataset_img.append(image)
-            dataset_label.append(label)
-        else:
-            testing_dataset_img.append(image)
-            testing_dataset_label.append(label)
+        dataset_img.append(load_image(path + '\\' + label + '\\' + f))
+        dataset_label.append(label)
         CPT += 1
         print(f'{CPT / FILENUMBER:2.2%} fichiers chargés')
 
 
-def load_dataset(train_ratio):
+def load_dataset(path):
     global CPT
     global FILENUMBER
     CPT = 0
     FILENUMBER = 0
-    labelnames = [f for f in os.listdir(PATH)]
-    training_dataset_img = []
-    training_dataset_label = []
-    testing_dataset_img = []
-    testing_dataset_label = []
-    threads = [None] * len(labelnames)
+    labelnames = [f for f in os.listdir(path)]
+    dataset_img = []
+    dataset_label = []
+    classes = [None] * len(labelnames)
 
-    for i in range(len(threads)):
-        FILENUMBER += len(fnmatch.filter(os.listdir(PATH + '\\' + labelnames[i] + '\\'), '*.*'))
-        threads[i] = threading.Thread(target=load_images, args=(labelnames[i], training_dataset_img, training_dataset_label,train_ratio, testing_dataset_img, testing_dataset_label))
-        threads[i].start()
-    for i in range(len(threads)):
-        threads[i].join()
+    for i in range(len(classes)):
+        FILENUMBER += len(fnmatch.filter(os.listdir(path + '\\' + labelnames[i] + '\\'), '*.*'))
+        classes[i] = threading.Thread(target=load_images, args=(
+            path, labelnames[i], dataset_img, dataset_label))
+        classes[i].start()
+    for i in range(len(classes)):
+        classes[i].join()
 
-    return training_dataset_img, training_dataset_label, testing_dataset_img, testing_dataset_label
+    return dataset_img, dataset_label
 
+
+"""
+training_dataset_img, training_dataset_label = load_dataset(os.path.realpath('../../DATASET_80px_TRAIN'))
+testing_dataset_img, testing_dataset_label = load_dataset(os.path.realpath('../../DATASET_80px_TEST'))
+size_first_layer = sum([numpy.prod(img.shape) for img in training_dataset_img])
+"""
